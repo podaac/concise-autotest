5903dd2636f016a639c13ffb25ab442c06301c8e
James Wood <James.F.Wood@jpl.nasa.gov>
2024-04-24 08:39:47 -0700

    add test_concatenate

--- tests/verify_collection.py
+++ tests/verify_collection.py
@@ -10,6 +10,7 @@ import harmony
 import netCDF4
 import numpy as np
 import podaac.subsetter.subset
+import unittest
 import pytest
 import requests
 import xarray
@@ -337,6 +338,7 @@ def get_lat_lon_var_names(dataset: xarray.Dataset, file_to_subset: str, collecti
 
 
 @pytest.mark.timeout(600)
+@pytest.mark.skip
 def test_spatial_subset(collection_concept_id, env, granule_json, collection_variables,
                         harmony_env, tmp_path: pathlib.Path, bearer_token):
     test_spatial_subset.__doc__ = f"Verify spatial subset for {collection_concept_id} in {env}"
@@ -475,3 +477,157 @@ def test_spatial_subset(collection_concept_id, env, granule_json, collection_var
             logging.info("Partial Lon Success - no Data")
         else:
             assert False
+
+
+def verify_dims(merged_group, origin_group, both_merged):
+    for dim in origin_group.dimensions:
+        if both_merged:
+            unittest.TestCase().assertEqual(merged_group.dimensions[dim].size, origin_group.dimensions[dim].size)
+        else:
+            unittest.TestCase().assertGreaterEqual(merged_group.dimensions[dim].size, origin_group.dimensions[dim].size)
+
+
+def verify_attrs(merged_obj, origin_obj, both_merged):
+    ignore_attributes = [
+        'request-bounding-box', 'request-bounding-box-description', 'PODAAC-dataset-shortname',
+        'PODAAC-persistent-ID', 'time_coverage_end', 'time_coverage_start'
+    ]
+
+    merged_attrs = merged_obj.ncattrs()
+    origin_attrs = origin_obj.ncattrs()
+
+    for attr in origin_attrs:
+        if attr in ignore_attributes:
+            # Skip attributes which are present in the Java implementation,
+            # but not (currently) present in the Python implementation
+            continue
+
+        if not both_merged and attr not in merged_attrs:
+            # Skip attributes which are not present in both merged and origin.
+            # This is normal operation as some attributes may be omited b/c
+            # they're inconsistent between granules
+            continue
+
+        merged_attr = merged_obj.getncattr(attr)
+        if both_merged and isinstance(merged_attr, int):
+            # Skip integer values - the Java implementation seems to omit
+            # these values due to its internal handling of all values as
+            # Strings
+            continue
+
+        origin_attr = origin_obj.getncattr(attr)
+        if isinstance(origin_attr, np.ndarray):
+            unittest.TestCase().assertTrue(np.array_equal(merged_attr, origin_attr))
+        else:
+            if attr != "history_json":
+                unittest.TestCase().assertEqual(merged_attr, origin_attr)
+
+
+def verify_variables(merged_group, origin_group, subset_index, both_merged):
+    for var in origin_group.variables:
+        merged_var = merged_group.variables[var]
+        origin_var = origin_group.variables[var]
+
+        verify_attrs(merged_var, origin_var, both_merged)
+
+        if both_merged:
+            # both groups require subset indexes
+            merged_data = merged_var[subset_index[0]]
+            origin_data = origin_var[subset_index[1]]
+        else:
+            # merged group requires a subset index
+            merged_data = np.resize(merged_var[subset_index], origin_var.shape)
+            origin_data = origin_var
+
+        equal_nan = True
+        if merged_data.dtype.kind == 'S':
+            equal_nan = False
+
+        # verify variable data
+        if isinstance(origin_data, str):
+            unittest.TestCase().assertEqual(merged_data, origin_data)
+        else:
+            unittest.TestCase().assertTrue(np.array_equal(merged_data, origin_data, equal_nan=equal_nan))
+
+
+def verify_groups(merged_group, origin_group, subset_index, file=None, both_merged=False):
+    if file:
+        print("verifying groups ....." + file)
+
+    verify_dims(merged_group, origin_group, both_merged)
+    verify_attrs(merged_group, origin_group, both_merged)
+    verify_variables(merged_group, origin_group, subset_index, both_merged)
+
+    for child_group in origin_group.groups:
+        merged_subgroup = merged_group[child_group]
+        origin_subgroup = origin_group[child_group]
+        verify_groups(merged_subgroup, origin_subgroup, subset_index, both_merged=both_merged)
+
+
+def download_file(url, local_path, headers):
+    response = requests.get(url, stream=True, headers=headers)
+    if response.status_code == 200:
+        with open(local_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print("Original File downloaded successfully. " + local_path)
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+
+@pytest.mark.timeout(600)
+def test_concatenate(collection_concept_id, harmony_env, bearer_token):
+
+    max_results = 2
+
+    harmony_client = harmony.Client(env=harmony_env, token=bearer_token)
+    collection = harmony.Collection(id=collection_concept_id)
+
+    request = harmony.Request(
+        collection=collection,
+        concatenate=True,
+        max_results=max_results,
+        skip_preview=True,
+        format="application/x-netcdf4",
+    )
+
+    request.is_valid()
+
+    print(harmony_client.request_as_curl(request))
+
+    job1_id = harmony_client.submit(request)
+
+    print(f'\n{job1_id}')
+
+    print(harmony_client.status(job1_id))
+
+    print('\nWaiting for the job to finish')
+
+    results = harmony_client.result_json(job1_id)
+
+    print('\nDownloading results:')
+
+    futures = harmony_client.download_all(job1_id)
+    file_names = [f.result() for f in futures]
+    print('\nDone downloading.')
+
+    filename = file_names[0]
+
+    # Handle time dimension and variables dropping
+    merge_dataset = netCDF4.Dataset(filename, 'r')
+
+    headers = {
+        "Authorization": f"Bearer {bearer_token}"
+    }
+
+    original_files = merge_dataset.variables['subset_files']
+    history_json = json.loads(merge_dataset.history_json)
+    assert len(original_files) == max_results
+
+    for url in history_json[0].get("derived_from"):
+        local_file_name = os.path.basename(url)
+        download_file(url, local_file_name, headers)
+
+    for i, file in enumerate(original_files):
+        origin_dataset = netCDF4.Dataset(file)
+        verify_groups(merge_dataset, origin_dataset, i, file=file)
